069c78c3e2024e254175375f31d200dd4dd179e8
chandni855 <108581984+chandni855@users.noreply.github.com>
2022-07-24 19:40:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+object=[];
 video="";
 status="";
 function preload()
@@ -13,6 +14,30 @@ function setup()
 function draw()
 {
     image(video,0,0,480,380);
+    if (status!="")
+    {
+        objectDetector.detect(video,gotresutls);
+        for(i=0 ; i<object.length ; i++)
+        {
+            document.getElementById("status").innerHTML="status : object detected";
+            document.getElementById("number_of_objects").innerHTML="number of objects detected are "+object.length;
+        }
+        fill("#FF0000");
+        percent=floor(object[i].confidence*100);
+        text(object[i].label+""+percent+"%",object[i].x+15,object[i].y+15);
+        nofill();
+        strock("#FF0000");
+        rect(object[i].x,object[i].y,object[i].width,object[i].height);
+    }
+}
+function gotresutls(error , result)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    console.log(result);
+    object=result;
 }
 function start()
 {
